f7e109018db59be5f6b5e5266206925fb5faf665
Alek Kruk <kruk.aleksander@hotmail.com>
2019-09-16 14:48:51 +0200

    Updates

---
+++ precache-manifest.f4b9182b92755b74aad5105557ca80f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b34695e27e59dd9e87cb07e4b9f5914",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "a5c987a81ed3364effa6",
+    "url": "/to-do-app/static/css/main.44a799ee.chunk.css"
+  },
+  {
+    "revision": "8ea52f7c99f2af9a6844",
+    "url": "/to-do-app/static/js/2.32985611.chunk.js"
+  },
+  {
+    "revision": "a5c987a81ed3364effa6",
+    "url": "/to-do-app/static/js/main.8b3bebdc.chunk.js"
+  },
+  {
+    "revision": "e810453f515726c6e33e",
+    "url": "/to-do-app/static/js/runtime~main.96842007.js"
+  }
+]);
